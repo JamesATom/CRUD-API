01c9908da756f007722d85838078be8084f4a02e
Abdulaziz <abduhakimovabdulaziz7@gmail.com>
2023-09-02 18:40:13 +0500

    fix: GET method of http works properly

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,18 +1,18 @@
 import http from 'node:http';
 import { UserDB } from '../models/db';
-import { REG_ENDPOINT_BASE, anythingAfterUsers } from '../regex/regex';
+import { REG_ENDPOINT_BASE, REG_UUID, anythingAfterUsers } from '../regex/regex';
 
 const router = (req: http.IncomingMessage, res: http.ServerResponse) => {
     const { method, url } = req;
     const splitUrl: string | undefined = url?.split('/')[3];
-    const fullUrl = `http://localhost:8000${url}`;
+    // const fullUrl = `http://localhost:8000${url}`;
     
     if (method === 'GET') {
         if (url === '/api/users') {
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.write(JSON.stringify({ message: UserDB }));
             res.end();
-        } else if (splitUrl) {
+        } else if (REG_UUID.test(splitUrl || '') && splitUrl) {
             const user = UserDB.find((user) => user.id === splitUrl);
             if (user) {
                 res.writeHead(200, { 'Content-Type': 'application/json' });
