46daee152da241b80ea59662766a2a92c7aab083
Abdulaziz <abduhakimovabdulaziz7@gmail.com>
2023-09-03 19:17:16 +0500

    feat: working on cluster

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import http from 'node:http';
 import { UserDB } from '../models/db';
 import { REG_ENDPOINT_BASE, REG_UUID, anythingAfterUsers } from '../regex/regex';
+import crypto from 'node:crypto';
 
 const router = (req: http.IncomingMessage, res: http.ServerResponse) => {
     const { method, url } = req;
@@ -29,7 +30,87 @@ const router = (req: http.IncomingMessage, res: http.ServerResponse) => {
             res.end();
         }
     } else if (method === 'POST') {
-
+        if (url === '/api/users') {
+            let body = '';
+            req.on('data', (chunk) => {
+                body += chunk.toString();
+            });
+            req.on('end', () => {
+                const { username, age, hobbies } = JSON.parse(body);
+                if (!username || !age || !hobbies) {
+                    res.writeHead(400, { 'Content-Type': 'application/json' });
+                    res.write(JSON.stringify({ message: 'Invalid request' }));
+                    res.end();
+                    return;
+                }
+                const newUser = {
+                    id: crypto.randomUUID(),
+                    username,
+                    age,
+                    hobbies
+                };
+                UserDB.push(newUser);
+                res.writeHead(201, { 'Content-Type': 'application/json' });
+                res.write(JSON.stringify({ message: newUser }));
+                res.end();
+            });
+        }
+    } else if (method === 'PUT') {
+        if (REG_UUID.test(splitUrl || '') && splitUrl) {
+            const user = UserDB.find((user) => user.id === splitUrl);
+            if (user) {
+                let body = '';
+                req.on('data', (chunk) => {
+                    body += chunk.toString();
+                });
+                req.on('end', () => {
+                    const { username, age, hobbies } = JSON.parse(body);
+                    if (!username || !age || !hobbies) {
+                        res.writeHead(400, { 'Content-Type': 'application/json' });
+                        res.write(JSON.stringify({ message: 'Invalid request' }));
+                        res.end();
+                        return;
+                    }
+                    user.username = username;
+                    user.age = age; 
+                    user.hobbies = hobbies;
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.write(JSON.stringify({ message: user }));
+                    res.end();
+                });
+            } else {
+                res.writeHead(404, { 'Content-Type': 'application/json' });
+                res.write(JSON.stringify({ message: 'User not found' }));
+                res.end();
+            }
+        } else {
+            res.writeHead(400, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify({ message: 'Invalid UserID' }));
+            res.end();
+        }
+    } else if (method === 'DELETE') {
+        if (REG_UUID.test(splitUrl || '') && splitUrl) {
+            const user = UserDB.find((user) => user.id === splitUrl);
+            if (user) {
+                const index = UserDB.indexOf(user);
+                UserDB.splice(index, 1);
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.write(JSON.stringify({ message: 'User deleted' }));
+                res.end();
+            } else {
+                res.writeHead(404, { 'Content-Type': 'application/json' });
+                res.write(JSON.stringify({ message: 'User not found' }));
+                res.end();
+            }
+        } else {
+            res.writeHead(400, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify({ message: 'Invalid UserID' }));
+            res.end();
+        }
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ message: 'Route not found' }));
+        res.end();
     }
 }
 
